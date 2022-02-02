e310c68c1d11add22d64dc7583c7c90652c2572f
Drew Nnodi <dnnodi@deltadentalmn.org>
2022-02-02 17:47:30 -0600

    Add Lazy Loading for Invoice Builder Module

--- src/app/invoice-builder/invoice-builder-routing.module.ts
+++ src/app/invoice-builder/invoice-builder-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {InvoicesComponent} from './invoices.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   {path: '', component: InvoicesComponent,},
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+   imports: [RouterModule.forChild(routes)],
+   exports: [RouterModule]
 })
-export class InvoiceBuilderRoutingModule { }
+export class InvoiceBuilderRoutingModule {
+}
