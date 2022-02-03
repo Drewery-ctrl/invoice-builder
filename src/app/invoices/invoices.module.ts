34236286444630bee951206ab1b82df3fe809d2b
Drew Nnodi <dnnodi@deltadentalmn.org>
2022-02-03 00:11:19 -0600

    Generate Invoices Module & Components

---
+++ src/app/invoices/invoices.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InvoiceListingComponent } from './components/invoice-listing/invoice-listing.component';
+
+
+
+@NgModule({
+  declarations: [
+    InvoiceListingComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class InvoicesModule { }
