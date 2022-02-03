ac4378306a70205852e8f4fc4c6dd8018da7ed46
Drew Nnodi <dnnodi@deltadentalmn.org>
2022-02-03 00:56:43 -0600

    Configure Babel to Transpile ES6 to ES5

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-const express = require('express');
+import express from 'express';
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Welcome to Invoice Builder Backend!');
