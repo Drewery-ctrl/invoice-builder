9ab41e98ba5528fbf006456f5115e01d1260c97a
Drew Nnodi <dnnodi@deltadentalmn.org>
2022-02-02 23:23:12 -0600

    Add Query to Close Side Navigation Bar at Max_Width 720px

--- src/app/invoice-builder/components/side-nav/side-nav.component.ts
+++ src/app/invoice-builder/components/side-nav/side-nav.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, NgZone, OnInit} from '@angular/core';
+
+const MAX_WIDTH_BREAKPOINT = 720;
 
 @Component({
-  selector: 'app-side-nav',
-  templateUrl: './side-nav.component.html',
-  styleUrls: ['./side-nav.component.scss']
+   selector: 'app-side-nav',
+   templateUrl: './side-nav.component.html',
+   styleUrls: ['./side-nav.component.scss']
 })
 export class SideNavComponent implements OnInit {
+   private mediaMatcher: any = matchMedia(`(max-width: ${MAX_WIDTH_BREAKPOINT}px`)
 
-  constructor() { }
+   constructor(zone: NgZone) {
+      this.mediaMatcher.addEventListener('change', (mql: any) => {
+         zone.run(() => this.mediaMatcher = mql);
+      });
+   }
 
-  ngOnInit(): void {
-  }
+   ngOnInit(): void {
+   }
 
+   isScreenSmall() {
+      return this.mediaMatcher.matches;
+   }
 }
