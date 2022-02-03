f7a9ed513efddd0f118b51c6ad2c378d1a07aeeb
Drew Nnodi <dnnodi@deltadentalmn.org>
2022-02-03 00:45:19 -0600

    Add babel-CLI & Babel-Preset-Env

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
+app.get('/', (req, res) => {
+  res.send('Welcome to Invoice Builder Backend!');
+});
 
 app.listen(PORT, () => {
    console.log(`Server listening on port ${PORT}`);
