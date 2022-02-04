71d6b3f7cf79997e4d206ff79b729cd04dbe0229
Drew Nnodi <dnnodi@deltadentalmn.org>
2022-02-03 18:30:29 -0600

    Add Express Middleware to Parse Request Body

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import logger from 'morgan';
 import { router } from './config/routes';
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(logger('dev'));
 app.use('/api/v1', router);
 const PORT = process.env.PORT || 3000;
