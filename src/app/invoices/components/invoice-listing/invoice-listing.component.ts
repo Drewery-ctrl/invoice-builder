dacc9b7cd1697afec0eda5807de46784a61609ca
Drew Nnodi <dnnodi@deltadentalmn.org>
2022-02-05 03:50:02 -0600

    Refresh DataTable After Deleting Records

--- src/app/invoices/components/invoice-listing/invoice-listing.component.ts
+++ src/app/invoices/components/invoice-listing/invoice-listing.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {InvoiceService} from "../../services/invoice.service";
 import {Invoice} from "../../models/invoice";
 import {Router} from "@angular/router";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {remove} from "lodash";
 
 @Component({
    selector: 'app-invoice-listing',
@@ -11,20 +13,38 @@ import {Router} from "@angular/router";
 export class InvoiceListingComponent implements OnInit {
    displayedColumns: string[] = ['ITEM', 'AMOUNT', 'QUANTITY', 'TAX', 'DATE', 'DUE DATE', 'ACTIONS'];
    dataSource: Invoice[] = [];
-   private _router: Router;
 
-   constructor(private invoiceService: InvoiceService, router: Router) {
-      this._router = router;
+   constructor(private invoiceService: InvoiceService, private _router: Router, private _snackBar: MatSnackBar) {
    }
 
    ngOnInit() {
       this.invoiceService.getInvoices().subscribe(invoices => {
          this.dataSource = invoices;
-         // console.log(invoices);
+      }, error => {
+         this.errorHandler(error, 'Failed to load invoices');
       });
    }
 
-   addInvoice() {
-     this._router.navigate(['dashboard', 'invoices', 'new']);
+   async addInvoice() {
+      await this._router.navigate(['dashboard', 'invoices', 'new']);
+   }
+
+   async deleteInvoice(id: string) {
+      await this.invoiceService.deleteInvoice(id).subscribe((invoice) => {
+         remove(this.dataSource, (item) => {
+            return item._id === invoice._id;
+         });
+         this.dataSource = [...this.dataSource];
+         this._snackBar.open('Invoice deleted', 'Success', {duration: 2000});
+      }, error => {
+         this.errorHandler(error, 'Error deleting invoice');
+      });
+   }
+
+   private errorHandler(error: any, displayMessage: string) {
+      console.log(error);
+      this._snackBar.open(displayMessage, 'Error', {
+         duration: 2000,
+      });
    }
 }
