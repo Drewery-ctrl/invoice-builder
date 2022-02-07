f49229defe06f405e39e3cca404fcb77a617d089
Drew Nnodi <dnnodi@deltadentalmn.org>
2022-02-07 15:19:13 -0600

    Add Client Mongoose Model

---
+++ src/api/resources/clients/client.model.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const clientSchema = new Schema({
+   firstName: { type: String, required: true },
+   lastName: { type: String, required: true },
+   email: { type: String, required: true },
+   phone: { type: Number, required: true },
+}, { timestamps: true });
+
+
+export default mongoose.model('Client', clientSchema);
