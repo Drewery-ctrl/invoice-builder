7cc0e051416df81743ffc3e6d6beb8c996b7de82
Drew Nnodi <dnnodi@deltadentalmn.org>
2022-02-08 00:23:36 -0600

    Add UpdateClient in Material Dialog

--- src/api/resources/clients/client.model.js
+++ src/api/resources/clients/client.model.js
@@ -6,7 +6,7 @@ const clientSchema = new Schema({
    firstName: { type: String, required: true },
    lastName: { type: String, required: true },
    email: { type: String, required: true, unique: true },
-   phone: { type: Number, required: true },
+   phone: { type: String, required: true },
 }, { timestamps: true });
 
 
