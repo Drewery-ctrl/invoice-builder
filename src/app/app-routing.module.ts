8bae7b3f25b507ec2d932f00bed17cc071a9e13a
Drew Nnodi <dnnodi@deltadentalmn.org>
2022-02-08 16:53:19 -0600

    Setup AuthModule for User Login/Signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AuthComponent} from './auth/auth.component';
 
 const routes: Routes = [
    {path: '', component: AppComponent, pathMatch: 'full'},
+   {path: 'login', component: AuthComponent, pathMatch: 'full'},
+   {path: 'signup', component: AuthComponent, pathMatch: 'full'},
    {path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)},
    {path: '**', redirectTo: 'dashboard', pathMatch: 'full'},
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {MaterialModule} from "./shared/material.module";
+import {AuthModule} from "./auth/auth.module";
 
 @NgModule({
    declarations: [
@@ -14,7 +15,8 @@ import {MaterialModule} from "./shared/material.module";
       BrowserModule,
       BrowserAnimationsModule,
       MaterialModule,
-      AppRoutingModule
+      AppRoutingModule,
+      AuthModule
    ],
    providers: [],
    bootstrap: [AppComponent]
