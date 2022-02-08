7a19466c4078e3945192cb5906bd45d13494c0b5
Drew Nnodi <dnnodi@deltadentalmn.org>
2022-02-08 17:32:01 -0600

    Setup LoginForm

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+   selector: 'app-auth',
+   templateUrl: './auth.component.html',
+   styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent implements OnInit {
+   authForm: FormGroup;
 
-  constructor() { }
+   constructor(private fb: FormBuilder) {
+   }
 
-  ngOnInit(): void {
-  }
+   ngOnInit(): void {
+      this.initForm();
+   }
 
+   private initForm() {
+      this.authForm = this.fb.group({
+         email: ['', Validators.required],
+         password: ['', Validators.required]
+      });
+   }
+
+   onSubmit() {
+      console.log('Submitted');
+   }
 }
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,15 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AuthComponent } from './auth.component';
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {AuthComponent} from './auth.component';
+import {MaterialModule} from "../shared/material.module";
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
-  declarations: [
-    AuthComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+   declarations: [
+      AuthComponent
+   ],
+   imports: [
+      CommonModule,
+      MaterialModule,
+      FormsModule,
+      ReactiveFormsModule
+   ]
 })
-export class AuthModule { }
+export class AuthModule {
+}
