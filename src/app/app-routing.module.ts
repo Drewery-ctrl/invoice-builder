a5826dc6a48715d601e7dd4e5f06ade24321b100
Drew Nnodi <dnnodi@deltadentalmn.org>
2022-02-08 20:54:05 -0600

    Add NoAuthGuards to Public Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {AppComponent} from './app.component';
 import {AuthComponent} from './auth/auth.component';
+import {NoAuthGuardService} from "./core/services/no-auth-guard.service";
 
 const routes: Routes = [
    {path: '', component: AppComponent, pathMatch: 'full'},
-   {path: 'login', component: AuthComponent, pathMatch: 'full'},
-   {path: 'signup', component: AuthComponent, pathMatch: 'full'},
+   {path: 'login', component: AuthComponent, pathMatch: 'full', canActivate: [NoAuthGuardService]},
+   {path: 'signup', component: AuthComponent, pathMatch: 'full', canActivate: [NoAuthGuardService]},
    {path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)},
    {path: '**', redirectTo: 'dashboard', pathMatch: 'full'},
 ];
