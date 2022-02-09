611fc717a2448e9ffae04c630550dd49f4f714fe
Drew Nnodi <dnnodi@deltadentalmn.org>
2022-02-08 23:08:11 -0600

    Implement Logout Feature. End Section 15

--- src/app/dashboard/components/tool-bar/tool-bar.component.ts
+++ src/app/dashboard/components/tool-bar/tool-bar.component.ts
@@ -1,4 +1,6 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Router} from '@angular/router';
+import {JwtService} from 'src/app/core/services/jwt.service';
 
 @Component({
    selector: 'app-tool-bar',
@@ -8,10 +10,15 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 export class ToolBarComponent implements OnInit {
    @Output() toggleSideNav = new EventEmitter<void>()
 
-   constructor() {
+   constructor(private jwtService: JwtService, private router: Router) {
    }
 
    ngOnInit(): void {
+   }
+
+   async logout() {
+      this.jwtService.destroyToken();
+      await this.router.navigate(['/login']);
    }
 
 }
