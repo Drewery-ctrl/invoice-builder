518444b8a3ca27581fadf465360c3cccb2f0933e
Drew Nnodi <dnnodi@deltadentalmn.org>
2022-02-11 02:22:09 -0600

    Add Sales Tax Logic to Invoice View

--- src/app/invoices/components/invoice-view/invoice-view.component.ts
+++ src/app/invoices/components/invoice-view/invoice-view.component.ts
@@ -9,6 +9,8 @@ import {Invoice} from '../../models/invoice';
 })
 export class InvoiceViewComponent implements OnInit {
    invoice: Invoice;
+   total: number;
+   salesTax = 0;
 
    constructor(private activatedRoute: ActivatedRoute) {
    }
@@ -17,6 +19,14 @@ export class InvoiceViewComponent implements OnInit {
       this.activatedRoute.data.subscribe(data => {
          this.invoice = data['invoice'];
          console.log(this.invoice);
+
+         if (typeof this.invoice.quantity !== "undefined" && typeof this.invoice.amount !== "undefined") {
+            this.total = this.invoice.quantity * this.invoice.amount;
+         }
+         if (typeof this.invoice.tax !== "undefined") {
+            this.salesTax = this.invoice.tax / 100 * this.total;
+         }
+         this.total += this.salesTax;
       })
    }
 
