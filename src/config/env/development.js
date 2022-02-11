1f503b127baf846b78637df3e242b0a8cb4240b3
Drew Nnodi <dnnodi@deltadentalmn.org>
2022-02-10 18:26:38 -0600

    Create New Twitter App & Add Config to Env file

--- src/config/env/development.js
+++ src/config/env/development.js
@@ -19,5 +19,13 @@ export const devConfig = {
       auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
       client_secret: "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",
       callbackURL: "http://localhost:3001/api/v1/auth/google/callback"
+   },
+   twitter: {
+      api_key: "nMxhOxNgvyplausVXNwdAv8nw",
+      api_key_secret: "tPycyAWWihhOMrXtfOmEgrhgGyfDg8JX9S1m7X461lyKMOq0B7",
+      bearer_token: "AAAAAAAAAAAAAAAAAAAAAGxoZAEAAAAAwiEnaq033QeCiTWxxFWeIiHQHRU%3Dwg8L0xf6qTyRVc4OmhKvIbCsnhT5AshA5a65v4EkA5d3WrPJka",
+      client_id: "TE9TSTZRMEFvWEczWDRhaE5xTEU6MTpjaQ",
+      client_secret: "D2dlsxEx_FZ_iBDqQtU62JOUoY7iHUJZ2OX3evEQT0dO02OkDo",
+      callbackURL: "http://localhost:3001/api/v1/auth/twitter/callback"
    }
 };
